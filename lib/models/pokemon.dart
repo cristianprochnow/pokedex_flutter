1bba613dae30a75aefb35d5d302e99730047c1fd
Cristian Prochnow <cristianprochnow@gmail.com>
2023-10-11 19:59:59 -0300

    feat: complete Pokemon app and add config file

--- lib/models/pokemon.dart
+++ lib/models/pokemon.dart
@@ -1,17 +1,24 @@
 class Pokemon {
-  String id;
   String url;
   String name;
 
   Pokemon({
-    required this.id,
     required this.url,
     required this.name
   });
 
-  String get image {
-    return
-      "https://raw.githubusercontent.com/PokeAPI/sprites/"
-      "master/sprites/pokemon/other/official-artwork/$id.png";
+  int get id => int.parse(
+    url
+      .split('/')
+      .reversed
+      .where((element) => element != "")
+      .first
+  );
+
+  String get image => "https://raw.githubusercontent.com/PokeAPI"
+    "/sprites/master/sprites/pokemon/other/official-artwork/$id.png";
+
+  factory Pokemon.fromJson(Map<String, dynamic> jsonData) {
+    return Pokemon(url: jsonData['url'], name: jsonData['name']);
   }
 }
