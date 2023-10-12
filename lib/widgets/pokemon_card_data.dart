fa91e27f229cc407b880e122af3f951ed9731c1c
Cristian Prochnow <cristianprochnow@gmail.com>
2023-10-11 22:23:52 -0300

    feat: add PokemonCardData widget styles

--- lib/widgets/pokemon_card_data.dart
+++ lib/widgets/pokemon_card_data.dart
@@ -12,6 +12,23 @@ class PokemonCardData extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Column(
+      children: [
+        Expanded(
+          child: Padding(
+            padding: const EdgeInsets.all(16),
+            child: Image.network(pokemon.image),
+          ),
+        ),
+        const Divider(),
+        Text(
+          pokemon.name.toString(),
+          style: const TextStyle(
+            fontSize: 21,
+            color: Colors.black87,
+          ),
+        ),
+      ],
+    );
   }
 }
